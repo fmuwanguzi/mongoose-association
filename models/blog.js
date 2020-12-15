425b9a455a78e1f6a46da1f626383bb69a009203
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-15 16:15:03 -0500

    [blog model] create blogPost model

---
+++ models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    header: String,
+    content: String,
+    date: Date
+})
+
+const blogPostSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    comments: [commentSchema]
+})
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = BlogPost;
+
+//Below is another way to export the BlogPost model
+// module.exports = mongoose.model('BlogPost', blogPostSchema);
